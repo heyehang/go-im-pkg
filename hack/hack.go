901866d90464675b23d779e2598d676343b7d123
huangshaodong <tomy145@qq.com>
2022-08-31 20:49:11 +0800

    update hack string

--- hack/hack.go
+++ hack/hack.go
@@ -18,10 +18,15 @@ func String(b []byte) (s string) {
 // no copy to change string to slice
 // use your own risk
 func Slice(s string) (b []byte) {
-	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	pbytes.Data = pstring.Data
-	pbytes.Len = pstring.Len
-	pbytes.Cap = pstring.Len
+	if s == "" {
+		return nil
+	}
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	b = *(*[]byte)(unsafe.Pointer(&bh))
 	return
 }
